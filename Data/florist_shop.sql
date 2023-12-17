7502852b3a3d13deebf272fdaac6e4489bef2736
Ernesto Arcos <arcosernesto@gmail.com>
2023-12-17 19:51:21 +0100

    Añado script de BBDD en data

---
+++ Data/florist_shop.sql
@@ -0,0 +1,214 @@
+CREATE DATABASE  IF NOT EXISTS `florist_shop` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `florist_shop`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: florist_shop
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `decoration`
+--
+
+DROP TABLE IF EXISTS `decoration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `decoration` (
+  `product_id` int NOT NULL,
+  `material` varchar(45) COLLATE utf8mb3_spanish_ci NOT NULL,
+  PRIMARY KEY (`product_id`),
+  CONSTRAINT `decoration_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`idproduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `decoration`
+--
+
+LOCK TABLES `decoration` WRITE;
+/*!40000 ALTER TABLE `decoration` DISABLE KEYS */;
+INSERT INTO `decoration` VALUES (4,'Plástico'),(8,'Madera');
+/*!40000 ALTER TABLE `decoration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `floristshop`
+--
+
+DROP TABLE IF EXISTS `floristshop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `floristshop` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb3_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `floristshop`
+--
+
+LOCK TABLES `floristshop` WRITE;
+/*!40000 ALTER TABLE `floristshop` DISABLE KEYS */;
+INSERT INTO `floristshop` VALUES (1,'Rosas del Valle'),(2,'Rosas del Bosque'),(3,'Decoflor');
+/*!40000 ALTER TABLE `floristshop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `flower`
+--
+
+DROP TABLE IF EXISTS `flower`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flower` (
+  `product_id` int NOT NULL,
+  `color` varchar(45) COLLATE utf8mb3_spanish_ci NOT NULL,
+  PRIMARY KEY (`product_id`),
+  CONSTRAINT `flower_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`idproduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flower`
+--
+
+LOCK TABLES `flower` WRITE;
+/*!40000 ALTER TABLE `flower` DISABLE KEYS */;
+INSERT INTO `flower` VALUES (1,'Rojo'),(2,'Amarillo'),(3,'Violeta'),(6,'Blanco'),(7,'Violeta'),(10,'Amarillo');
+/*!40000 ALTER TABLE `flower` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `idproduct` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb3_spanish_ci NOT NULL,
+  `price` double NOT NULL,
+  `category_id` int NOT NULL,
+  `floristshop_id` int NOT NULL,
+  `ticket_id` int DEFAULT NULL,
+  PRIMARY KEY (`idproduct`),
+  KEY `category_id_idx` (`category_id`),
+  KEY `floristshop_id_idx` (`floristshop_id`),
+  KEY `ticket_id_idx` (`ticket_id`),
+  CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`),
+  CONSTRAINT `floristshop_id` FOREIGN KEY (`floristshop_id`) REFERENCES `floristshop` (`id`),
+  CONSTRAINT `ticket_id` FOREIGN KEY (`ticket_id`) REFERENCES `ticket` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Rosa Roja',10.99,2,1,NULL),(2,'Girasol Amarillo',8.99,2,1,NULL),(3,'Ramito de Tulipanes',15.99,2,1,NULL),(4,'Adorno Floral',12.5,3,1,NULL),(5,'Árbol de Navidad',29.99,1,1,1),(6,'Bouquet de Rosas Blancas',18.99,2,2,NULL),(7,'Orquídea Morada',24.99,2,2,NULL),(8,'Centro de Mesa Floral',22.5,3,2,2),(9,'Pequeño Árbol Decorativo',14.99,1,2,NULL),(10,'Lirios Amarillos',12.5,2,2,2);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_category`
+--
+
+DROP TABLE IF EXISTS `product_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb3_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_category`
+--
+
+LOCK TABLES `product_category` WRITE;
+/*!40000 ALTER TABLE `product_category` DISABLE KEYS */;
+INSERT INTO `product_category` VALUES (1,'Tree'),(2,'Flower'),(3,'Decoration');
+/*!40000 ALTER TABLE `product_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `floristshop_id` int NOT NULL,
+  `total_price` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `floristshop_id_idx` (`floristshop_id`),
+  KEY `ticket_florist_id_idx` (`floristshop_id`),
+  CONSTRAINT `ticket_florist_id` FOREIGN KEY (`floristshop_id`) REFERENCES `floristshop` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+INSERT INTO `ticket` VALUES (1,1,29.99),(2,2,35);
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tree`
+--
+
+DROP TABLE IF EXISTS `tree`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tree` (
+  `product_id` int NOT NULL,
+  `height` double NOT NULL,
+  PRIMARY KEY (`product_id`),
+  CONSTRAINT `tree_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`idproduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tree`
+--
+
+LOCK TABLES `tree` WRITE;
+/*!40000 ALTER TABLE `tree` DISABLE KEYS */;
+INSERT INTO `tree` VALUES (5,1.7),(9,0.5);
+/*!40000 ALTER TABLE `tree` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-17 19:47:27
